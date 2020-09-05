72bab2e5dd71c2980dda1d0ae6daf288d24e8cd3
Vladyslav <vlad.polishuk.007@gmail.com>
2020-09-05 17:25:26 +0300

    commiting.

--- src/GreetingsWorld.java
+++ src/GreetingsWorld.java
@@ -2,5 +2,6 @@ public class GreetingsWorld {
     public static void main(String[] args) {
         System.out.println("Hello Marina Sergeevna");
         System.out.println("I'm so proud of this community!");
+        System.out.println("third line");
     }
 }
