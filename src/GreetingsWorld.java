6107a21e2a87e323d4e6a490044ecc5e0e642223
Vladyslav <vlad.polishuk.007@gmail.com>
2020-09-16 22:23:13 +0300

    New string appear

--- src/GreetingsWorld.java
+++ src/GreetingsWorld.java
@@ -4,5 +4,6 @@ public class GreetingsWorld {
         System.out.println("I'm so proud of this community!");
         System.out.println("third line");
         System.out.println("another line in master branch");
+        System.out.println("Line in new branch");
     }
 }
